62eb3e12a09dc34eabda75a791e3420eee7fab86
SmileJosiah <zhuxingyi@aliyun.com>
2020-04-09 21:47:55 +0800

    添加Pojo User

---
+++ src/main/java/com/three/lonely/pojo/User.java
@@ -0,0 +1,27 @@
+package com.three.lonely.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+/**
+ * @program: lonely
+ * @author: Josiah
+ * @create: 2020-04-09 21:38
+ **/
+@Entity
+@Getter
+@Setter
+public class User {
+
+    private Integer id;
+
+    private String userName;
+    private String userPhone;
+    private String userSex;
+    private String userImage;
+    private String userBirthday;
+
+
+}
